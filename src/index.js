0072ddfa8fc2f36eeb1328aae5438058d0394f78
Fabio <fabiog@live.com.pt>
2020-07-22 00:04:18 -0400

    import product category

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Greeting from "components/Greeting";
 import 'css/index.css';
+import Product from "components/product";
 // import test from 'img/300-test.png';
 
 const Header = () =>{
@@ -19,56 +20,55 @@ const App = () => {
     //Fetch product listing
     const productData = [
 
-    {
-        // imgPath:'smartwatch-large.jpg',
-        Id:1,
-        Title:"Fitbit Versa 2",
-        Description:"Fitbit Versa 2 Health And Fitness Smartwatch",
-        Price:`$263`,
-        
-    },
- {
-        // imgPath:'airpods.jpg',
-        Id:2,
-        Title:"Apple AirPods",
-        Description:"Apple MMEF2AM/A AirPods Wireless Bluetooth Headset for iPhone",
-        Price:`$250`,
-        
-    },
-    {
-        // imgPath:'home-camera.jpg',
-        Id:3,
-        Title:"YI 1080p Home Camera",
-        Description:"Indoor Wireless IP Security Surveillance System with Night Vision",
-        Price:`$35`,
-        
-    },
-    {
-        // imgPath:'drone.jpg',
-        Id:4,
-        Title:"DJI Mavic Mini",
-        Description:"2.7K Camera, Controller, 3-Axis Gimbal, GPS, 30 Minutes Flight Time",
-        Price:`$499`,
-        
-    },
-    {
-        // imgPath:'canon-camera.jpg',
-        Id:5,
-        Title:"Canon EOS M50",
-        Description:"Mirrorless Camera Kit with 15-45mm lens(Black)",
-        Price:`$729`,
-        
-    }
-
-    ]
-const productArray = productData.map(details => <product key={details.Id} title={details.Title} description={details.Description} price={details.Price}/>)
+        {
+            // imgPath:'smartwatch-large.jpg',
+            Id:1,
+            Title:"Fitbit Versa 2",
+            Description:"Fitbit Versa 2 Health And Fitness Smartwatch",
+            Price:`$263`,
+            
+        },
+     {
+            // imgPath:'airpods.jpg',
+            Id:2,
+            Title:"Apple AirPods",
+            Description:"Apple MMEF2AM/A AirPods Wireless Bluetooth Headset for iPhone",
+            Price:`$250`,
+            
+        },
+        {
+            // imgPath:'home-camera.jpg',
+            Id:3,
+            Title:"YI 1080p Home Camera",
+            Description:"Indoor Wireless IP Security Surveillance System with Night Vision",
+            Price:`$35`,
+            
+        },
+        {
+            // imgPath:'drone.jpg',
+            Id:4,
+            Title:"DJI Mavic Mini",
+            Description:"2.7K Camera, Controller, 3-Axis Gimbal, GPS, 30 Minutes Flight Time",
+            Price:`$499`,
+            
+        },
+        {
+            // imgPath:'canon-camera.jpg',
+            Id:5,
+            Title:"Canon EOS M50",
+            Description:"Mirrorless Camera Kit with 15-45mm lens(Black)",
+            Price:`$729`,
+            
+        }
+    
+        ]
+    const productArray = productData.map(details => <Product key={details.Id} title={details.Title} description={details.Description} price={details.Price}/>)
 
 	return (
         <>
-    {/* <Greeting name="Products" /> */}
+    <Greeting name="Products" />
     {/* <Header /> */}
-    <p>{productArray}</p>
-    
+    {/* <Product prop={productArray}/> */}    
     
     </>
     )
---
+++ src/js/components/product.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Product =({prop})=>{
+
+    return (
+<>
+    <p>Title:{prop}</p>
+    {/* <p>Description:{prop}</p>
+    <p>{prop}</p> */}
+    </>
+    )
+}
+
+export default Product;
