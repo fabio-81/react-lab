377db9d010e64734bbc398c1275d1bc6a2a9baec
Fabio <fabiog@live.com.pt>
2020-07-22 00:04:41 -0400

    remove hello string

--- src/js/components/Greeting.js
+++ src/js/components/Greeting.js
@@ -1,11 +1,11 @@
 import React from "react";
-import test from 'img/300-test.png';
+// import test from 'img/300-test.png';
 
 const Greeting = ({name}) => {
 	return (
     <>
-    <h1>Hello, {name}!</h1>
-    <img src={test} alt="test image" />
+    <h1>{name}!</h1>
+    {/* <img src={test} alt="test image" /> */}
     </>
     )}
 
