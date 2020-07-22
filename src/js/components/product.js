32a015f29549296b29d1e658a689586fd6f3c467
Fabio <fabiog@live.com.pt>
2020-07-22 15:55:24 -0400

    add title, description and price headings

--- src/js/components/product.js
+++ src/js/components/product.js
@@ -3,16 +3,20 @@ import React from "react";
 const Product =(prop)=>{
 
     return(
+     
 <>
+    
     <div>
         {/* <p>{prop.imgPath}</p> */}
-        <p>{prop.title}</p>
-        <p>{prop.description}</p>
-        <p>{prop.price}</p>
+        <p><b>Title:</b> {prop.title}</p>
+        <p><b>Description:</b> {prop.description}</p>
+        <p><b>Price:</b> {prop.price}</p>
     </div>
-
+    
 </>
+
     )
+    
 }
 
 export default Product;
