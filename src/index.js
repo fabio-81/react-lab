6051f94d08ffcbbe9837073a27bd3995285692b5
Fabio <fabiog@live.com.pt>
2020-07-26 13:05:09 -0400

    add imgPath property to productData array object and import images

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import 'css/index.css';
 import Product from "components/product";
 import Header from "components/Header"
 import Footer from "components/Footer"
+import Image from "img/airpods.jpg"
+import Image1 from "img/canon-camera.jpg"
+import Image2 from "img/drone.jpg"
+import Image3 from "img/home-camera.jpg"
+import Image4 from "img/smartwatch-large.jpg"
 // import test from 'img/300-test.png';
 // import Image1 from "img/smartwatch-large.jpg"
 // import test from 'img/300-test.png';
@@ -32,14 +37,14 @@ const App = () => {
         {
             
             Id:1,
-            // imgPath:'img/300-test.png',
+            imgPath:<img src={Image4} alt="Just a test image" />,
             Title:"Fitbit Versa 2",
             Description:"Fitbit Versa 2 Health And Fitness Smartwatch",
             Price:`$263`
             
         },
         {
-            // imgPath:'airpods.jpg',
+            imgPath:<img src={Image} alt="Just a test image" />,
             Id:2,
             Title:"Apple AirPods",
             Description:"Apple MMEF2AM/A AirPods Wireless Bluetooth Headset for iPhone",
@@ -47,7 +52,7 @@ const App = () => {
             
         },
         {
-            // imgPath:'home-camera.jpg',
+            imgPath:<img src={Image3} alt="Just a test image" />,
             Id:3,
             Title:"YI 1080p Home Camera",
             Description:"Indoor Wireless IP Security Surveillance System with Night Vision",
@@ -55,7 +60,7 @@ const App = () => {
             
         },
         {
-            // imgPath:'drone.jpg',
+            imgPath:<img src={Image2} alt="Just a test image" />,
             Id:4,
             Title:"DJI Mavic Mini",
             Description:"2.7K Camera, Controller, 3-Axis Gimbal, GPS, 30 Minutes Flight Time",
@@ -63,7 +68,7 @@ const App = () => {
             
         },
         {
-            // imgPath:'canon-camera.jpg',
+            imgPath:<img src={Image1} alt="Just a test image" />,
             Id:5,
             Title:"Canon EOS M50",
             Description:"Mirrorless Camera Kit with 15-45mm lens(Black)",
@@ -73,7 +78,7 @@ const App = () => {
     
         ]
     
-    const productArray =productData.map(details => <Product key={details.Id} title={details.Title} description={details.Description} price={details.Price}/>)
+    const productArray =productData.map(details => <Product key={details.Id} title={details.Title} description={details.Description} price={details.Price} image={details.imgPath}/>)
 
 	return (
         <>    
